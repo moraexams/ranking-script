d99779b4e7239106b7d7a1203d34d7120d957983
Sahithyan <sahithyan2701@gmail.com>
2023-12-17 11:21:55 +0530

    support both local and hosted db

--- src/db.ts
+++ src/db.ts
@@ -16,18 +16,38 @@ export const applicants = sqliteTable("tbl_students", {
 // 	nic: string;
 // };
 
-if (
-	process.env.PUBLIC_TURSO_DATABASE_URL == undefined ||
-	process.env.PUBLIC_TURSO_DATABASE_AUTH_TOKEN == undefined
-) {
-	console.warn(
-		"Turso-specific environment variables are not defined in process.env."
-	);
+if (process.env.IS_LOCAL == undefined) {
+	console.warn("env.IS_LOCAL is undefined. Treating is as false.");
+}
+
+if (process.env.IS_LOCAL) {
+	if (process.env.LOCAL_DB_FILE == undefined) {
+		console.warn("env.LOCAL_DB_FILE is undefined");
+		process.exit(1);
+	}
+} else {
+	if (
+		process.env.PUBLIC_TURSO_DATABASE_URL == undefined ||
+		process.env.PUBLIC_TURSO_DATABASE_AUTH_TOKEN == undefined
+	) {
+		console.warn(
+			"Turso-specific environment variables are not defined in process.env."
+		);
+		console.log(
+			"Define env.PUBLIC_TURSO_DATABASE_URL, env.PUBLIC_TURSO_DATABASE_AUTH_TOKEN to resolve this issue."
+		);
+		process.exit(1);
+	}
 }
 
 export const client = createClient({
-	url: "file:./me22.db",
-	syncUrl: process.env.PUBLIC_TURSO_DATABASE_URL,
+	// @ts-expect-error
+	url: process.env.IS_LOCAL
+		? `file:${process.env.LOCAL_DB_FILE}`
+		: process.env.PUBLIC_TURSO_DATABASE_URL,
+	syncUrl: process.env.IS_LOCAL
+		? process.env.PUBLIC_TURSO_DATABASE_URL
+		: undefined,
 	authToken: process.env.PUBLIC_TURSO_DATABASE_AUTH_TOKEN,
 });
 
