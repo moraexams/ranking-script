4bdeb1c3ecfb196f097d417ea447a3b85b445aa5
Sahithyan <sahithyan2701@gmail.com>
2023-12-21 06:03:36 +0530

    add batch export script

---
+++ run.sh
@@ -0,0 +1,11 @@
+$id="_0601"
+bun run export-as-csv --subject maths --part part1 --id $id
+bun run export-as-csv --subject maths --part part2 --id $id
+bun run export-as-csv --subject bio --part part1 --id $id
+bun run export-as-csv --subject bio --part part2 --id $id
+bun run export-as-csv --subject chemistry --part part1 --id $id
+bun run export-as-csv --subject chemistry --part part2 --id $id
+bun run export-as-csv --subject physics --part part1 --id $id
+bun run export-as-csv --subject physics --part part2 --id $id
+bun run export-as-csv --subject ict --part part1 --id $id
+bun run export-as-csv --subject ict --part part2 --id $id
